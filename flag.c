e8ee4b49bbb81250a2d145a2eb2e87015b9fc23d
shabani005 <hasan.shabani2005@gmail.com>
2025-10-03 19:19:49 +0300

    first commit

---
+++ flag.c
@@ -0,0 +1,51 @@
+#define NB_IMPLEMENTATION
+#include "nb.h"
+
+typedef struct {
+  char**    label;
+  char**     desc;
+  size_t    count;
+  size_t capacity;
+} nb_flags;
+
+void nb_flag_append_impl(nb_flags *flags, char *label, char *desc){
+  if (flags->capacity == 0){
+    flags->capacity = 128;
+    flags->label = malloc(sizeof(char*)*flags->capacity);
+    flags->desc = malloc(sizeof(char*)*flags->capacity);
+  } if (flags->count >= flags->capacity){
+    flags->capacity *=2;
+    flags->label = realloc(flags->label, sizeof(char*) * flags->capacity);
+    flags->desc  = realloc(flags->desc, sizeof(char*) * flags->capacity); 
+  }
+  flags->label[flags->count] = strdup(label);
+  flags->desc[flags->count] = strdup(desc);
+  flags->count++;
+}
+
+void nb_flag(nb_flags *flags, int argc, char** argv){
+  if (flags->count > 0){
+    if (argc < 2){
+      for (size_t i=0; i<flags->count; ++i){
+        if (!flags->desc[i]) flags->desc[i] = "no given desc"; // impossible case
+        printf("-%s -> %s \n", flags->label[i], flags->desc[i]);
+      }
+    } else {
+      // here add logic for parsing flags
+      for (size_t i=0; i<argc; ++i){
+        //printf("%zu\n", i);
+        if (argv[i][0] == '-' ){ // && argv[i][1] == '-'          //printf("found {-} in %s\n", argv[i]);
+          memmove(argv[i], argv[i]+1, strlen(argv[i]));
+          // in python it would be if flags->labels[argv[i]] != null
+       } 
+      }     
+    }
+  }     
+}
+
+int main(int argc, char **argv){
+  nb_flags flags = {0};
+  nb_flag_append_impl(&flags, "help", "display other commands");
+  nb_flag(&flags, argc, argv);
+  printf("%c\n", flags.label[0][0]);
+}
