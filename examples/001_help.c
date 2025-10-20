1c51d6b1110ce0ffd9442ff843512ed561cf7e16
shabani005 <hasan.shabani2005@gmail.com>
2025-10-20 14:32:30 +0300

    improved example

--- examples/001_help.c
+++ examples/001_help.c
@@ -1,13 +1,13 @@
 #define FG_IMPLEMENTATION
 #include "../flag.h"
 
-void helpt(){
+void helpf(){
   printf("help func ptr works!\n");
 }
 
 int main(int argc, char** argv){
   fg_flags flags = {0};
-  fg_append_ptr(&flags, "help", helpt);
+  fg_append_ptr(&flags, "help", helpf);
   fg_append(&flags, "run", "runs file");
   fg_append(&flags, "rm", "removes file");
   fg_run(&flags, argc, argv);
